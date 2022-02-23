22f680a9bb19430de5ce9eaeb86526aa9fdb23e9
chingaloEric <echingalo@gmail.com>
2022-02-23 15:33:48 +0300

    feat(modules/dhis): added the controller for different api endpoints

--- src/modules/dhis/controllers/dhis/dhis.controller.ts
+++ src/modules/dhis/controllers/dhis/dhis.controller.ts
@@ -1,4 +1,186 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Put,
+  HttpException,
+  Param,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { find } from 'lodash';
+import { DhisService } from '../../services/dhis/dhis.service';
 
-@Controller('dhis')
-export class DhisController {}
+@Controller('')
+export class DhisController {
+  cache = {};
+  allowedEndPoints = ['me.json', 'dataStore'];
+
+  constructor(private dhisService: DhisService) {}
+  @Get(':endPoint')
+  async getAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => param.endPoint.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      const results = await this.dhisService.getAPI(
+        request,
+        param,
+        query,
+        body,
+      );
+      response.status(200);
+      response.send(results);
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+
+  @Get(':endPoint/*')
+  async getExtendedPointAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const path = decodeURI(request.url).split('/api/').join('');
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => path.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      if (this.cache[path]) {
+        response.status(200);
+        response.send(this.cache[path]);
+      } else {
+        const results = await this.dhisService.getAPI(
+          request,
+          param,
+          query,
+          body,
+        );
+        response.status(200);
+        response.send(results);
+        this.cache[path] = results;
+      }
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+
+  @Post(':endPoint')
+  async postAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => param.endPoint.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      const results = await this.dhisService.getAPI(
+        request,
+        param,
+        query,
+        body,
+      );
+      response.status(200);
+      response.send(results);
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+
+  @Post(':endPoint/*')
+  async postExtendedPointAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const path = decodeURI(request.url).split('/api/').join('');
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => path.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      const results = await this.dhisService.getAPI(
+        request,
+        param,
+        query,
+        body,
+      );
+      response.status(200);
+      response.send(results);
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+
+  @Put(':endPoint')
+  async putAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => param.endPoint.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      const results = await this.dhisService.getAPI(
+        request,
+        param,
+        query,
+        body,
+      );
+      response.status(200);
+      response.send(results);
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+
+  @Put(':endPoint/*')
+  async putExtendedPointAPI(
+    @Req() request,
+    @Param() param,
+    @Query() query,
+    @Res() response,
+    @Body() body?,
+  ): Promise<any> {
+    const path = decodeURI(request.url).split('/api/').join('');
+    const allowedEndPoint = find(
+      this.allowedEndPoints,
+      (endPoint) => path.indexOf(endPoint) !== -1,
+    );
+    if (allowedEndPoint) {
+      const results = await this.dhisService.getAPI(
+        request,
+        param,
+        query,
+        body,
+      );
+      response.status(200);
+      response.send(results);
+    } else {
+      throw new HttpException('Not Found', 404);
+    }
+  }
+}
