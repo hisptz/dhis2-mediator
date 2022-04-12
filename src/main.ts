43fdbd43dd40100fe44ba06d563bba9f71b4865f
megamind <echingalo@gmail.com>
2022-04-12 13:56:20 +0300

    fix: 🐛 appended the global prefix as 'api'

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './modules/dhis/utilities/exception.filter';
 
 async function bootstrap() {
+  const globalPrefix = 'api';
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
-
+  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+  console.log(`Mediator now available at localhost:${port}/${globalPrefix}`);
 }
 bootstrap();
