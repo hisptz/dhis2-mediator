8c4cc09f8704c2791d58e5a1a5b3181bde8f5d3b
megamind <echingalo@gmail.com>
2023-02-18 17:15:25 +0300

    build: added a build package.json file

--- build-mediator.sh
+++ build-mediator.sh
@@ -6,6 +6,6 @@ cp ./Dockerfile ./dist/
 cp ./docker-compose.yml ./dist/docker-compose.yml
 cp ./.env.example ./dist/.env.example
 cp ./README.md ./dist/README.md
-cp ./package.json ./dist/package.json
+cp ./package-build.json ./dist/package.json
 cd ./dist/ || return
 zip -r api.zip .
