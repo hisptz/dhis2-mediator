5161d15ab45d2fca9ef8bd60f55f4dd7c646f697
megamind <echingalo@gmail.com>
2023-06-12 23:51:56 +0300

    refactor: register the cache module on dhis2 module

--- src/modules/dhis/dhis.module.ts
+++ src/modules/dhis/dhis.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DhisService } from './services/dhis/dhis.service';
 import { DhisController } from './controllers/dhis/dhis.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   providers: [ConfigService, DhisService],
   controllers: [DhisController],
   exports: [DhisService],
